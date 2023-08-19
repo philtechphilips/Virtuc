3962e45c8f279fa2b6cbe7c98a0eff61c5fbca38
philtechphilips <69679124+philtechphilips@users.noreply.github.com>
2023-08-19 20:22:15 +0100

    feat: shop page implemented

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import Home from './screens/Home'
 import 'remixicon/fonts/remixicon.css'
 import { Route, Routes } from 'react-router-dom'
+import Shop from './screens/Shop'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/shop" element={<Shop />}></Route>
       </Routes>
     </>
   )
---
+++ src/components/User/Shop/Product.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ProductCard from '../Home/Cards/ProductCard'
+import { products } from '../../../assets/data/data'
+
+const Products = () => {
+ 
+
+  return (
+    <div className='flex flex-col items-center'>
+      <div className='homepage-categories-container bg-white w-full'>
+        <div className='flex items-center justify-between'>
+          <h1 className='homepage-categories-container-heading p-600 text-xl md:text-2xl'>All Products</h1>
+        </div>
+        <div className='flex flex-wrap justify-start gap-1 md:gap-4 mt-7 w-full'>
+          {products.map((product, index) => (
+            <ProductCard product={product} key={index} />
+          ))}
+        </div>
+      </div>
+      <button className='p-500 md:mt-[0px] mb-24  border w-fit border-[#6FCEAD] rounded px-5 py-2 hover:bg-[#6FCEAD] hover:text-white text-[#6FCEAD]'>View more</button>
+    </div>
+  )
+}
+
+
+export default Products
